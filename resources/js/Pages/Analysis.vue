d9b2efe5ab1a922a167f36c22fc0332a4443823c
mari-byte <zhendaolongtai26@gmail.com>
2023-04-16 22:28:46 +0900

    first

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -15,32 +15,33 @@ const form = reactive({
     startDate: null,
     endDate: null,
     type: "perDay",
+    rfmPrms: [14, 28, 60, 90, 7, 5, 3, 2, 300000, 200000, 100000, 30000],
 });
 
 const data = reactive({});
 
 const getData = async () => {
-    try {
-        await axios
-            .get("/api/analysis/", {
-                params: {
-                    startDate: form.startDate,
-                    endDate: form.endDate,
-                    type: form.type,
-                },
-            })
-            .then((res) => {
-                // data.value = res.data
-                data.data = res.data.data
-                data.labels = res.data.labels
-                data.totals = res.data.totals
-                data.type = res.data.type
-                console.log(res.data);
-            });
-    } catch (e) {
-        console.log(e.message);
+try{
+  await axios.get('/api/analysis/', {
+    params: {
+      startDate: form.startDate,
+      endDate: form.endDate,
+      type: form.type,
+      rfmPrms: form.rfmPrms
     }
-};
+  })
+  .then( res => {
+      data.data = res.data.data
+      if(res.data.labels) {data.labels = res.data.labels}
+      if(res.data.eachCount) {data.eachCount = res.data.eachCount}
+      data.totals = res.data.totals
+      data.type = res.data.type
+      console.log(res.data)
+    })
+  } catch (e){
+    console.log(e.message)
+  }
+}
 </script>
 
 <template>
@@ -58,12 +59,34 @@ const getData = async () => {
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         <form @submit.prevent="getData">
-                            分析方法<br>
-                            <input type="radio" v-model="form.type" value="perDay" checked><span class="mr-2">日別</span>
-                            <input type="radio" v-model="form.type" value="perMonth"><span class="mr-2">月別</span>
-                            <input type="radio" v-model="form.type" value="perYear"><span class="mr-2">年別</span>
-                            <input type="radio" v-model="form.type" value="decile"><span class="mr-2">デシル分析</span>
-                            <br>
+                            分析方法<br />
+                            <input
+                                type="radio"
+                                v-model="form.type"
+                                value="perDay"
+                                checked
+                            /><span class="mr-2">日別</span>
+                            <input
+                                type="radio"
+                                v-model="form.type"
+                                value="perMonth"
+                            /><span class="mr-2">月別</span>
+                            <input
+                                type="radio"
+                                v-model="form.type"
+                                value="perYear"
+                            /><span class="mr-2">年別</span>
+                            <input
+                                type="radio"
+                                v-model="form.type"
+                                value="decile"
+                            /><span class="mr-2">デシル分析</span>
+                            <input
+                                type="radio"
+                                v-model="form.type"
+                                value="rfm"
+                            /><span class="mr-2">RFM分析</span>
+                            <br />
                             From:
                             <input
                                 type="date"
@@ -76,6 +99,45 @@ const getData = async () => {
                                 name="endDate"
                                 v-model="form.endDate"
                             /><br />
+
+                            <div v-if="form.type === 'rfm'" class="my-8">
+                                <table class="mx-auto"> 
+                                    <thead> 
+                                    <tr> 
+                                    <th>ランク</th> 
+                                    <th>R (○日以内)</th> 
+                                    <th>F (○回以上)</th> 
+                                    <th>M (○円以上)</th> 
+                                    </tr> 
+                                    </thead> 
+                                    <tbody> 
+                                    <tr> 
+                                    <td>5</td> 
+                                    <td><input type="number" v-model="form.rfmPrms[0]"></td> 
+                                    <td><input type="number" v-model="form.rfmPrms[4]"></td> 
+                                    <td><input type="number" v-model="form.rfmPrms[8]"></td> 
+                                    </tr>
+                                    <tr> 
+                                    <td>4</td> 
+                                    <td><input type="number" v-model="form.rfmPrms[1]"></td> 
+                                    <td><input type="number" v-model="form.rfmPrms[5]"></td> 
+                                    <td><input type="number" v-model="form.rfmPrms[9]"></td> 
+                                    </tr>
+                                    <tr> 
+                                    <td>3</td> 
+                                    <td><input type="number" v-model="form.rfmPrms[2]"></td> 
+                                    <td><input type="number" v-model="form.rfmPrms[6]"></td> 
+                                    <td><input type="number" v-model="form.rfmPrms[10]"></td> 
+                                    </tr>
+                                    <tr> 
+                                    <td>2</td> 
+                                    <td><input type="number" v-model="form.rfmPrms[3]"></td> 
+                                    <td><input type="number" v-model="form.rfmPrms[7]"></td> 
+                                    <td><input type="number" v-model="form.rfmPrms[11]"></td> 
+                                    </tr>
+                                    </tbody>
+                                    </table>
+                            </div>
                             <button
                                 class="mt-4 flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
                             >
@@ -83,10 +145,11 @@ const getData = async () => {
                             </button>
                         </form>
                         <div v-show="data.data">
-                        <Chart :data="data"/>
-                        <ResultTable :data="data"/>
-                    </div>
-                        
+                            <div v-if="data.type != 'rfm' ">
+                            <Chart :data="data" />
+                          </div>
+                            <ResultTable :data="data" />
+                        </div>
                     </div>
                 </div>
             </div>
